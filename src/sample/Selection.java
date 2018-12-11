1de4834a4799dc7101c24520ae6e9e9bf78487ee
kmalki <kamelmalk96@gmail.com>
2018-12-11 14:14:51 +0100

    missing files

---
+++ src/sample/Selection.java
@@ -0,0 +1,78 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Selection {
+    private Controleur monControleur;
+    private ControleurIHMFX monControleurIHMFX;
+    @FXML
+    ComboBox<String> listeDeroulante;
+
+
+    public static Selection selectionner(Controleur controleur, ControleurIHMFX c, Stage laStageUnique) throws FileNotFoundException {
+        URL location = Menu.class.getResource("/views/Selection.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
+        Parent root = null;
+        try {
+            root = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        Selection vueS = fxmlLoader.getController();
+        vueS.setMonControleurIHMFX(c);
+        vueS.setMonControleur(controleur);
+        laStageUnique.setTitle("Selection");
+        laStageUnique.setScene(new Scene(root, laStageUnique.getWidth(), laStageUnique.getHeight()));
+        laStageUnique.setMaximized(true);
+        laStageUnique.show();
+        vueS.remplirListeNiveau();
+        return vueS;
+    }
+
+    private void remplirListeNiveau() {
+        ArrayList<Niveau> l=this.monControleur.getNiveaux();
+        for (Niveau n : l
+        ) {
+            listeDeroulante.getItems().add(n.getName());
+        }
+    }
+
+
+    private void setMonControleurIHMFX(ControleurIHMFX c) {
+        monControleurIHMFX=c;
+    }
+    private void setMonControleur(Controleur c) {
+        monControleur=c;
+    }
+
+    public void goToMenu(ActionEvent event) {
+        this.monControleurIHMFX.goToMenu();
+    }
+
+    public void jouer() throws FileNotFoundException {
+        String levelName = listeDeroulante.getValue();
+        if (levelName != null) {
+            int numLevel = -1;
+            for (int i = 0; i < this.monControleur.getNiveaux().size(); i++) {
+                if (levelName == this.monControleur.getNiveaux().get(i).getName()) {
+                    numLevel = i;
+                }
+            }
+            this.monControleurIHMFX.jouer(numLevel);
+        }
+    }
+}
